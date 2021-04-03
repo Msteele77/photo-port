2ac51927e1e8e4414e443f869b8a93798f7074d3
Macheya Steele <mcheysteele@gmail.com>
2021-04-03 16:36:55 -0600

    added contact, updating tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
+const [contactSelected, setContactSelected] = useState(false);
 
 function App() {
   const [categories] = useState([
@@ -24,9 +26,19 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+      <>
+      <Gallery currentCategory={currentCategory}></Gallery>
+      <About></About>
+      </>
+      ) : (
+    <ContactForm></ContactForm>
+  )}
+
+
+
       </main>
+      
     </div>
   );
 }
